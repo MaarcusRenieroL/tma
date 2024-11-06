0371ddbe469af9c4ec960e7a70bab9a9b4413f9f
MaarcusRenieroL <maarcusreniero.l@gmail.com>
2024-11-07 05:19:36 +0530

    feat: re setup frontend project

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -1,10 +1,12 @@
 import { TestBed } from "@angular/core/testing";
+import { RouterModule } from "@angular/router";
 import { AppComponent } from "./app.component";
 
 describe("AppComponent", () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [RouterModule.forRoot([])],
+      declarations: [AppComponent],
     }).compileComponents();
   });
 
@@ -14,7 +16,7 @@ describe("AppComponent", () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'frontend' title`, () => {
+  it(`should have as title 'frontend'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual("frontend");
@@ -25,7 +27,7 @@ describe("AppComponent", () => {
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector("h1")?.textContent).toContain(
-      "Hello, frontend"
+      "Hello, frontend",
     );
   });
 });
