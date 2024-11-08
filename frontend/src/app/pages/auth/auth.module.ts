b225be5125a5e12401f42af01c05e4f96754cfed
MaarcusRenieroL <maarcusreniero.l@gmail.com>
2024-11-08 12:55:49 +0530

    feat: add sign up page

--- frontend/src/app/pages/auth/auth.module.ts
+++ frontend/src/app/pages/auth/auth.module.ts
@@ -5,15 +5,20 @@ import { CardModule } from "primeng/card";
 import { ButtonModule } from "primeng/button";
 import { InputTextModule } from "primeng/inputtext";
 import { PasswordModule } from "primeng/password";
+import { SignUpComponent } from "./sign-up/sign-up.component";
+import { AutoCompleteModule } from "primeng/autocomplete";
+import { FormsModule } from "@angular/forms";
 
 @NgModule({
-  declarations: [SignInComponent],
+  declarations: [SignInComponent, SignUpComponent],
   imports: [
     CommonModule,
     CardModule,
     InputTextModule,
     PasswordModule,
     ButtonModule,
+    AutoCompleteModule,
+    FormsModule,
   ],
 })
 export class AuthModule {}
