17a687ab1bc22c55023f6513859a056b5352305f
MaarcusRenieroL <maarcusreniero.l@gmail.com>
2024-11-15 07:06:24 +0530

    feat: setup project microservice

---
+++ backend/project-micro-service/src/main/java/com/tma/project_micro_service/ProjectMicroServiceApplication.java
@@ -0,0 +1,14 @@
+package com.tma.project_micro_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ProjectMicroServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ProjectMicroServiceApplication.class, args);
+  }
+}
