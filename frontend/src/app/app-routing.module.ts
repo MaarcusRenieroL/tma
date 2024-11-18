f012b365f0e0a3c4400500ac8e2ad4df3f941a2e
MaarcusRenieroL <maarcusreniero.l@gmail.com>
2024-11-18 10:28:07 +0530

    feat: add reset password page

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { LandingPageComponent } from "./pages/landing-page/landing-page.component";
 import { SignInComponent } from "./pages/auth-pages/sign-in/sign-in.component";
 import { SignUpComponent } from "./pages/auth-pages/sign-up/sign-up.component";
+import { PasswordResetComponent } from "./pages/auth-pages/password-reset/password-reset.component";
 
 const routes: Routes = [
   { path: "", component: LandingPageComponent },
   { path: "auth/sign-in", component: SignInComponent },
-  { path: "auth/sign-up", component: SignUpComponent }
+  { path: "auth/sign-up", component: SignUpComponent },
+  { path: "auth/reset-password", component: PasswordResetComponent },
 ];
 
 @NgModule({
--- frontend/src/app/pages/auth-pages/auth-pages.module.ts
+++ frontend/src/app/pages/auth-pages/auth-pages.module.ts
@@ -3,17 +3,21 @@ import { CommonModule } from '@angular/common';
 import { SignInComponent } from './sign-in/sign-in.component';
 import { ComponentsModule } from "../../components/components.module";
 import { SignUpComponent } from './sign-up/sign-up.component';
+import { PasswordResetComponent } from './password-reset/password-reset.component';
+import { RouterLink } from "@angular/router";
 
 
 
 @NgModule({
   declarations: [
     SignInComponent,
-    SignUpComponent
+    SignUpComponent,
+    PasswordResetComponent
   ],
-  imports: [
-    CommonModule,
-    ComponentsModule
-  ]
+	imports: [
+		CommonModule,
+		ComponentsModule,
+		RouterLink
+	]
 })
 export class AuthPagesModule { }
