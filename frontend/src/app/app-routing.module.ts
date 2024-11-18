192aaaf1da76ed9565afce908cbfa71a6341ecb9
MaarcusRenieroL <maarcusreniero.l@gmail.com>
2024-11-18 10:03:25 +0530

    feat: add sign up page

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LandingPageComponent } from "./pages/landing-page/landing-page.component";
 import { SignInComponent } from "./pages/auth-pages/sign-in/sign-in.component";
+import { SignUpComponent } from "./pages/auth-pages/sign-up/sign-up.component";
 
 const routes: Routes = [
   { path: "", component: LandingPageComponent },
-  { path: "auth/sign-in", component: SignInComponent }
+  { path: "auth/sign-in", component: SignInComponent },
+  { path: "auth/sign-up", component: SignUpComponent }
 ];
 
 @NgModule({
--- frontend/src/app/pages/auth-pages/auth-pages.module.ts
+++ frontend/src/app/pages/auth-pages/auth-pages.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SignInComponent } from './sign-in/sign-in.component';
 import { ComponentsModule } from "../../components/components.module";
+import { SignUpComponent } from './sign-up/sign-up.component';
 
 
 
 @NgModule({
   declarations: [
-    SignInComponent
+    SignInComponent,
+    SignUpComponent
   ],
   imports: [
     CommonModule,
