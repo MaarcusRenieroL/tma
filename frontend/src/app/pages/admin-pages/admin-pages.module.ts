a625b1b3e3fa9b95b714835ed5b8d8d8886aecae
MaarcusRenieroL <maarcusreniero.l@gmail.com>
2024-11-18 16:14:47 +0530

    feat: add ongoing tasks component for admin dashboard

--- frontend/src/app/pages/admin-pages/admin-pages.module.ts
+++ frontend/src/app/pages/admin-pages/admin-pages.module.ts
@@ -5,12 +5,14 @@ import { AdminNavbarComponent } from './admin-navbar/admin-navbar.component';
 import { ComponentsModule } from "../../components/components.module";
 import { NgIcon, NgIconsModule } from "@ng-icons/core";
 import { heroUser } from '@ng-icons/heroicons/outline';
+import { bootstrapEye, bootstrapPen, bootstrapTrash } from '@ng-icons/bootstrap-icons';
 import { AdminDashboardStatsComponent } from './admin-dashboard/admin-dashboard-stats/admin-dashboard-stats.component';
+import { AdminDashboardOngoingTasksComponent } from './admin-dashboard/admin-dashboard-ongoing-tasks/admin-dashboard-ongoing-tasks.component';
 
 
 @NgModule({
-	declarations: [ AdminDashboardComponent, AdminNavbarComponent, AdminNavbarComponent, AdminDashboardStatsComponent ],
-	imports: [ CommonModule, ComponentsModule, NgIconsModule.withIcons({ heroUser }) ],
+	declarations: [ AdminDashboardComponent, AdminNavbarComponent, AdminNavbarComponent, AdminDashboardStatsComponent, AdminDashboardOngoingTasksComponent ],
+	imports: [ CommonModule, ComponentsModule, NgIconsModule.withIcons({ heroUser, bootstrapEye, bootstrapPen, bootstrapTrash }) ],
 	exports: [ AdminDashboardComponent, AdminNavbarComponent ],
 })
 export class AdminPagesModule {
