5e578c7d24c730abde39c554fec6f7ee65668301
MaarcusRenieroL <maarcusreniero.l@gmail.com>
2024-11-18 17:45:31 +0530

    feat: add top performers component for admin dashboard

--- frontend/src/app/pages/admin-pages/admin-pages.module.ts
+++ frontend/src/app/pages/admin-pages/admin-pages.module.ts
@@ -8,10 +8,11 @@ import { heroUser } from '@ng-icons/heroicons/outline';
 import { bootstrapEye, bootstrapPen, bootstrapTrash } from '@ng-icons/bootstrap-icons';
 import { AdminDashboardStatsComponent } from './admin-dashboard/admin-dashboard-stats/admin-dashboard-stats.component';
 import { AdminDashboardOngoingTasksComponent } from './admin-dashboard/admin-dashboard-ongoing-tasks/admin-dashboard-ongoing-tasks.component';
+import { AdminDashboardTopPerformersComponent } from './admin-dashboard/admin-dashboard-top-performers/admin-dashboard-top-performers.component';
 
 
 @NgModule({
-	declarations: [ AdminDashboardComponent, AdminNavbarComponent, AdminNavbarComponent, AdminDashboardStatsComponent, AdminDashboardOngoingTasksComponent ],
+	declarations: [ AdminDashboardComponent, AdminNavbarComponent, AdminNavbarComponent, AdminDashboardStatsComponent, AdminDashboardOngoingTasksComponent, AdminDashboardTopPerformersComponent ],
 	imports: [ CommonModule, ComponentsModule, NgIconsModule.withIcons({ heroUser, bootstrapEye, bootstrapPen, bootstrapTrash }) ],
 	exports: [ AdminDashboardComponent, AdminNavbarComponent ],
 })
