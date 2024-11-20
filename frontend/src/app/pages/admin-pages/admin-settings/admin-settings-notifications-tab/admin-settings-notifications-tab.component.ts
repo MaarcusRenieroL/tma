cdc688328144c13b1779dcc17ab8031e417bd025
MaarcusRenieroL <maarcusreniero.l@gmail.com>
2024-11-20 14:45:12 +0530

    feat: add responsiveness to all tab contents

--- frontend/src/app/pages/admin-pages/admin-settings/admin-settings-notifications-tab/admin-settings-notifications-tab.component.ts
+++ frontend/src/app/pages/admin-pages/admin-settings/admin-settings-notifications-tab/admin-settings-notifications-tab.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'admin-settings-notifications-tab',
   templateUrl: './admin-settings-notifications-tab.component.html',
-  styleUrl: './admin-settings-notifications-tab.component.css'
 })
 export class AdminSettingsNotificationsTabComponent {
 
