d4c58a36ee6505e4d063e707a1603b4e495fd82b
MaarcusRenieroL <maarcusreniero.l@gmail.com>
2024-11-30 07:16:56 +0530

    feat: integrate frontend and backend for forgot password functionality

--- frontend/src/app/pages/auth-pages/forgot-password/forgot-password.component.ts
+++ frontend/src/app/pages/auth-pages/forgot-password/forgot-password.component.ts
@@ -1,9 +1,31 @@
 import { Component } from '@angular/core';
+import { AuthService } from "../../../services/auth/auth.service";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { toast } from "ngx-sonner";
 
 @Component({
   selector: 'auth-forgot-password',
   templateUrl: './forgot-password.component.html',
 })
 export class ForgotPasswordComponent {
-
+  resetPasswordForm: FormGroup;
+  email!: string;
+  
+  constructor(private formBuilder: FormBuilder, private authService: AuthService) {
+    this.resetPasswordForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+    });
+  }
+  
+  onSubmit() {
+    if (this.resetPasswordForm.valid) {
+      this.authService.forgotPassword({ email: this.resetPasswordForm.value.email }).subscribe((response) => {
+        if (response) {
+          toast.success("Check your inbox for email");
+        } else {
+          toast.error("Request failed")
+        }
+      })
+    }
+  }  
 }
--- frontend/src/app/pages/auth-pages/password-reset/password-reset.component.ts
+++ frontend/src/app/pages/auth-pages/password-reset/password-reset.component.ts
@@ -1,9 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
   selector: 'auth-password-reset',
   templateUrl: './password-reset.component.html',
 })
-export class PasswordResetComponent {
+export class PasswordResetComponent implements OnInit {
+  
+  constructor(private route: ActivatedRoute) {
+  }
+  
+  resetPasswordToken!: string;
+  
+  ngOnInit(): void {
+    this.resetPasswordToken = this.route.snapshot.queryParamMap.get("token")!;
+  }
+  
+  onSubmit() {
+    console.log(this.resetPasswordToken)
+  }
 
 }
