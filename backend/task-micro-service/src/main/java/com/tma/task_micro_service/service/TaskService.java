a8d8cd6755e59ba1e6d42280ecaa3e8a59ae7cc8
AkashR0211 <akashravindran02@gmail.com>
2024-12-06 00:46:05 +0530

    feat: refactored code for task service

--- backend/task-micro-service/src/main/java/com/tma/task_micro_service/service/TaskService.java
+++ backend/task-micro-service/src/main/java/com/tma/task_micro_service/service/TaskService.java
@@ -1,23 +1,27 @@
 package com.tma.task_micro_service.service;
 
 import com.tma.task_micro_service.model.Task;
+import com.tma.task_micro_service.payload.response.StandardResponse;
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.ResponseEntity;
 import java.util.List;
 import java.util.UUID;
 
 public interface TaskService {
-
-  List<Task> getAllTasks();
-
-  Task createTask(Task task, UUID userId, HttpServletRequest request);
-
-  Task updateTask(UUID taskId, Task task);
-
-  void deleteTask(UUID taskId);
-
-  Task getTaskById(UUID taskId);
-  List<Task> getTaskByUserId(UUID userId);
-  List<Task> getTaskByTeamId(UUID teamId);
-  List<Task> getTaskByProjectId(UUID projectId);
+  
+  ResponseEntity<StandardResponse<List<Task>>> getAllTasks(HttpServletRequest request);
+  
+  ResponseEntity<StandardResponse<Task>> createTask(Task task, UUID userId, HttpServletRequest request);
+  
+  ResponseEntity<StandardResponse<Task>> updateTask(UUID taskId, Task task, HttpServletRequest request);
+  
+  ResponseEntity<StandardResponse<Void>> deleteTask(UUID taskId, HttpServletRequest request);
+  
+  ResponseEntity<StandardResponse<Task>> getTaskById(UUID taskId, HttpServletRequest request);
+  
+  ResponseEntity<StandardResponse<List<Task>>> getTaskByUserId(UUID userId, HttpServletRequest request);
+  
+  ResponseEntity<StandardResponse<List<Task>>> getTaskByTeamId(UUID teamId, HttpServletRequest request);
+  
+  ResponseEntity<StandardResponse<List<Task>>> getTaskByProjectId(UUID projectId, HttpServletRequest request);
 }
-
