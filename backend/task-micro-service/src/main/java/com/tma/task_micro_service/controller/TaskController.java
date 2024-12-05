4cf6e96acb2be1f0fd925fa714628afaf5973bad
AkashR0211 <akashravindran02@gmail.com>
2024-12-06 00:53:07 +0530

    feat: refactored code for task service implementation and controller

--- backend/task-micro-service/src/main/java/com/tma/task_micro_service/controller/TaskController.java
+++ backend/task-micro-service/src/main/java/com/tma/task_micro_service/controller/TaskController.java
@@ -4,170 +4,63 @@ import com.tma.task_micro_service.model.Task;
 import com.tma.task_micro_service.payload.request.CreateTaskRequest;
 import com.tma.task_micro_service.payload.response.StandardResponse;
 import com.tma.task_micro_service.service.TaskService;
-import com.tma.task_micro_service.util.ResponseUtil;
 import jakarta.servlet.http.HttpServletRequest;
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/tasks")
 public class TaskController {
-
+  
   @Autowired private TaskService taskService;
-  private ResponseUtil responseUtil;
-
+  
   @GetMapping
   public ResponseEntity<StandardResponse<List<Task>>> getAllTasks(HttpServletRequest request) {
-    List<Task> tasks = taskService.getAllTasks();
-
-    if (tasks.isEmpty()) {
-      return responseUtil.buildErrorMessage(
-          HttpStatus.NOT_FOUND, "No tasks found", request, LocalDateTime.now());
-    }
-
-    return responseUtil.buildSuccessMessage(
-        HttpStatus.OK, "Tasks retrieved successfully", tasks, request, LocalDateTime.now());
+    return taskService.getAllTasks(request);
   }
-
+  
   @PostMapping
   public ResponseEntity<StandardResponse<Task>> createTask(
-      @RequestBody CreateTaskRequest taskRequest, HttpServletRequest request) {
-    try {
-
-      if (taskRequest.getTask().getTitle() == null
-          || taskRequest.getTask().getDescription() == null
-          || taskRequest.getTask().getDueDate() == null
-          || taskRequest.getTask().getPriority() == null) {
-        return responseUtil.buildErrorMessage(
-            HttpStatus.BAD_REQUEST, "Missing required fields", request, LocalDateTime.now());
-      }
-
-      Task createdTask =
-          taskService.createTask(taskRequest.getTask(), taskRequest.getUserId(), request);
-
-      return responseUtil.buildSuccessMessage(
-          HttpStatus.CREATED,
-          "Task created successfully",
-          createdTask,
-          request,
-          LocalDateTime.now());
-    } catch (Exception e) {
-
-      return responseUtil.buildErrorMessage(
-          HttpStatus.INTERNAL_SERVER_ERROR,
-          "An error occurred while creating the task",
-          request,
-          LocalDateTime.now());
-    }
+    @RequestBody CreateTaskRequest taskRequest, HttpServletRequest request) {
+    return taskService.createTask(taskRequest.getTask(), taskRequest.getUserId(), request);
   }
-
+  
   @PutMapping("/{taskId}")
-  public ResponseEntity<StandardResponse<Task>> updateUser(
-      @PathVariable UUID taskId, @RequestBody Task task, HttpServletRequest request) {
-    try {
-
-      if (taskService.getTaskById(taskId) == null) {
-        return responseUtil.buildErrorMessage(
-            HttpStatus.NOT_FOUND,
-            "Task not found with ID: " + taskId,
-            request,
-            LocalDateTime.now());
-      }
-
-      Task updatedTask = taskService.updateTask(taskId, task);
-
-      return responseUtil.buildSuccessMessage(
-          HttpStatus.OK, "Task updated successfully", updatedTask, request, LocalDateTime.now());
-
-    } catch (Exception e) {
-
-      return responseUtil.buildErrorMessage(
-          HttpStatus.INTERNAL_SERVER_ERROR,
-          "An error occurred while updating the task",
-          request,
-          LocalDateTime.now());
-    }
+  public ResponseEntity<StandardResponse<Task>> updateTask(
+    @PathVariable UUID taskId, @RequestBody Task task, HttpServletRequest request) {
+    return taskService.updateTask(taskId, task, request);
   }
-
+  
   @DeleteMapping("/{taskId}")
-  public ResponseEntity<StandardResponse<Task>> deleteTask(
-      @PathVariable UUID taskId, HttpServletRequest request) {
-    try {
-
-      if (taskService.getTaskById(taskId) == null) {
-        return responseUtil.buildErrorMessage(
-            HttpStatus.NOT_FOUND,
-            "Task not found with ID: " + taskId,
-            request,
-            LocalDateTime.now());
-      }
-
-      taskService.deleteTask(taskId);
-
-      return responseUtil.buildSuccessMessage(
-          HttpStatus.NO_CONTENT, "Task deleted successfully", null, request, LocalDateTime.now());
-
-    } catch (Exception e) {
-
-      return responseUtil.buildErrorMessage(
-          HttpStatus.INTERNAL_SERVER_ERROR,
-          "An error occurred while deleting the task",
-          request,
-          LocalDateTime.now());
-    }
+  public ResponseEntity<StandardResponse<Void>> deleteTask(
+    @PathVariable UUID taskId, HttpServletRequest request) {
+    return taskService.deleteTask(taskId, request);
   }
-
+  
   @GetMapping("/{taskId}")
   public ResponseEntity<StandardResponse<Task>> getTaskById(
-      @PathVariable UUID taskId, HttpServletRequest request) {
-    Task task = taskService.getTaskById(taskId);
-
-    if (task == null) {
-      return responseUtil.buildErrorMessage(
-          HttpStatus.NOT_FOUND, "Task not found with ID: " + taskId, request, LocalDateTime.now());
-    }
-
-    return responseUtil.buildSuccessMessage(
-        HttpStatus.OK, "Task retrieved successfully", task, request, LocalDateTime.now());
+    @PathVariable UUID taskId, HttpServletRequest request) {
+    return taskService.getTaskById(taskId, request);
   }
+  
   @GetMapping("user/{userId}")
-  public ResponseEntity<StandardResponse<List<Task>>>getTasksByUserId(@PathVariable UUID userId,HttpServletRequest request){
-    List<Task>tasks= taskService.getTaskByUserId(userId);
-    if (tasks.isEmpty()) {
-      return responseUtil.buildErrorMessage(
-        HttpStatus.NOT_FOUND, "No tasks found", request, LocalDateTime.now());
-    }
-    
-    return responseUtil.buildSuccessMessage(
-      HttpStatus.OK, "Tasks retrieved successfully", tasks, request, LocalDateTime.now());
+  public ResponseEntity<StandardResponse<List<Task>>> getTasksByUserId(
+    @PathVariable UUID userId, HttpServletRequest request) {
+    return taskService.getTaskByUserId(userId, request);
   }
   
   @GetMapping("team/{teamId}")
-  public ResponseEntity<StandardResponse<List<Task>>>getTasksByTeamId(@PathVariable UUID teamId, HttpServletRequest request){
-    List<Task>tasks= taskService.getTaskByTeamId(teamId);
-    if (tasks.isEmpty()) {
-      return responseUtil.buildErrorMessage(
-        HttpStatus.NOT_FOUND, "No tasks found", request, LocalDateTime.now());
-    }
-    
-    return responseUtil.buildSuccessMessage(
-      HttpStatus.OK, "Tasks retrieved successfully", tasks, request, LocalDateTime.now());
-  }
-  @GetMapping("project/{projectId}")
-  public ResponseEntity<StandardResponse<List<Task>>>getTasksByProjectId(@PathVariable UUID projectId, HttpServletRequest request){
-    List<Task>tasks= taskService.getTaskByUserId(projectId);
-    if (tasks.isEmpty()) {
-      return responseUtil.buildErrorMessage(
-        HttpStatus.NOT_FOUND, "No tasks found", request, LocalDateTime.now());
-    }
-    
-    return responseUtil.buildSuccessMessage(
-      HttpStatus.OK, "Tasks retrieved successfully", tasks, request, LocalDateTime.now());
+  public ResponseEntity<StandardResponse<List<Task>>> getTasksByTeamId(
+    @PathVariable UUID teamId, HttpServletRequest request) {
+    return taskService.getTaskByTeamId(teamId, request);
   }
   
+  @GetMapping("project/{projectId}")
+  public ResponseEntity<StandardResponse<List<Task>>> getTasksByProjectId(
+    @PathVariable UUID projectId, HttpServletRequest request) {
+    return taskService.getTaskByProjectId(projectId, request);
+  }
 }
--- backend/task-micro-service/src/main/java/com/tma/task_micro_service/service/TaskServiceImpl.java
+++ backend/task-micro-service/src/main/java/com/tma/task_micro_service/service/TaskServiceImpl.java
@@ -2,97 +2,130 @@ package com.tma.task_micro_service.service;
 
 import com.tma.task_micro_service.feign.UserFeignClient;
 import com.tma.task_micro_service.model.Task;
+import com.tma.task_micro_service.payload.response.StandardResponse;
 import com.tma.task_micro_service.repository.TaskRepository;
+import com.tma.task_micro_service.util.ResponseUtil;
 import jakarta.servlet.http.HttpServletRequest;
-import java.util.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.*;
 
 @Service
 public class TaskServiceImpl implements TaskService {
-  @Autowired TaskRepository taskRepo;
-  @Autowired UserFeignClient userFeignClient;
-
+  
+  @Autowired
+  TaskRepository taskRepo;
+  
+  @Autowired
+  UserFeignClient userFeignClient;
+  
   @Override
-  public List<Task> getAllTasks() {
-    return taskRepo.findAll();
+  public ResponseEntity<StandardResponse<List<Task>>> getAllTasks(HttpServletRequest request) {
+    List<Task> tasks = taskRepo.findAll();
+    return ResponseUtil.buildSuccessMessage(HttpStatus.OK, "Tasks fetched successfully", tasks, request, LocalDateTime.now());
   }
-
+  
   @Override
-  public Task createTask(Task task, UUID userId, HttpServletRequest request) {
-
+  public ResponseEntity<StandardResponse<Task>> createTask(Task task, UUID userId, HttpServletRequest request) {
+    
     String bearerToken = request.getHeader("Authorization");
-
+    
     if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
-      
-      if(task.getUserIds()==null) {
+      if (task.getUserIds() == null) {
         Set<UUID> userIds = new HashSet<>();
         userIds.add(userId);
         task.setUserIds(userIds);
+      } else {
+        task.getUserIds().add(userId);
       }
-      else {
-       Set<UUID> userIds=task.getUserIds();
-       userIds.add(userId);
-       task.setUserIds(userIds);
-       
-       
-      }
-      Task savedTeam = taskRepo.save(task);
-
-      userFeignClient.addTaskToUser(savedTeam.getTeamId(), userId, bearerToken);
-
-      return savedTeam;
+      
+      Task savedTask = taskRepo.save(task);
+      userFeignClient.addTaskToUser(savedTask.getTeamId(), userId, bearerToken);
+      
+      return ResponseUtil.buildSuccessMessage(HttpStatus.CREATED, "Task created successfully", savedTask, request, LocalDateTime.now());
     }
-
-    return null;
-  }
-
-  @Override
-  public Task updateTask(UUID taskId, Task task) {
-    if (!taskRepo.existsById(taskId)) {
-      return null;
-    }
-
-    Optional<Task> optionalTask = taskRepo.findById(taskId);
-
-    if (optionalTask.isPresent()) {
-      Task task1 = optionalTask.get();
-      task1.setTitle(task.getTitle());
-      task1.setStatus(task.getStatus());
-      task1.setPriority(task.getPriority());
-      task1.setDescription(task.getDescription());
-      task1.setDueDate(task.getDueDate());
-      task1.setDateAllocated(task.getDateAllocated());
-
-      taskRepo.save(task1);
-
-      return task1;
-    }
-
-    return null;
-  }
-
-  public void deleteTask(UUID taskId) {
-    taskRepo.deleteById(taskId);
-  }
-
-  public Task getTaskById(UUID taskId) {
-    return taskRepo.findById(taskId).orElse(null);
-  }
-  
-  @Override
-  public List<Task> getTaskByUserId(UUID userId) {
-    return taskRepo.findTaskByUserIds(userId);
-  }
-  
-  @Override
-  public List<Task> getTaskByTeamId(UUID teamId) {
-    return taskRepo.findByTeamId(teamId);
-  }
-  
-  @Override
-  public List<Task> getTaskByProjectId(UUID projectId) {
-    return taskRepo.findByProjectId(projectId);
     
+    return ResponseUtil.buildErrorMessage(HttpStatus.UNAUTHORIZED, "Invalid or missing bearer token", request, LocalDateTime.now());
   }
+  
+  @Override
+  public ResponseEntity<StandardResponse<Task>> updateTask(UUID taskId, Task task, HttpServletRequest request) {
+    if (!taskRepo.existsById(taskId)) {
+      return ResponseUtil.buildErrorMessage(HttpStatus.NOT_FOUND, "Task not found", request, LocalDateTime.now());
+    }
+    
+    Optional<Task> optionalTask = taskRepo.findById(taskId);
+    
+    if (optionalTask.isPresent()) {
+      Task existingTask = optionalTask.get();
+      existingTask.setTitle(task.getTitle());
+      existingTask.setStatus(task.getStatus());
+      existingTask.setPriority(task.getPriority());
+      existingTask.setDescription(task.getDescription());
+      existingTask.setDueDate(task.getDueDate());
+      existingTask.setDateAllocated(task.getDateAllocated());
+      
+      taskRepo.save(existingTask);
+      
+      return ResponseUtil.buildSuccessMessage(HttpStatus.OK, "Task updated successfully", existingTask, request, LocalDateTime.now());
+    }
+    
+    return ResponseUtil.buildErrorMessage(HttpStatus.NOT_FOUND, "Task not found", request, LocalDateTime.now());
+  }
+  
+  @Override
+  public ResponseEntity<StandardResponse<Void>> deleteTask(UUID taskId, HttpServletRequest request) {
+    if (!taskRepo.existsById(taskId)) {
+      return ResponseUtil.buildErrorMessage(HttpStatus.NOT_FOUND, "Task not found", request, LocalDateTime.now());
+    }
+    
+    taskRepo.deleteById(taskId);
+    return ResponseUtil.buildSuccessMessage(HttpStatus.OK, "Task deleted successfully", null, request, LocalDateTime.now());
+  }
+  
+  @Override
+  public ResponseEntity<StandardResponse<Task>> getTaskById(UUID taskId, HttpServletRequest request) {
+    Task task = taskRepo.findById(taskId).orElse(null);
+    
+    if (task == null) {
+      return ResponseUtil.buildErrorMessage(HttpStatus.NOT_FOUND, "Task not found", request, LocalDateTime.now());
+    }
+    
+    return ResponseUtil.buildSuccessMessage(HttpStatus.OK, "Task retrieved successfully", task, request, LocalDateTime.now());
+  }
+  
+  @Override
+  public ResponseEntity<StandardResponse<List<Task>>> getTaskByUserId(UUID userId, HttpServletRequest request) {
+    List<Task> tasks = taskRepo.findTaskByUserIds(userId);
+    if(tasks.isEmpty()){
+      return ResponseUtil.buildErrorMessage(HttpStatus.NOT_FOUND, "Task not found", request, LocalDateTime.now());
+      
+    }
+    return ResponseUtil.buildSuccessMessage(HttpStatus.OK, "Tasks fetched successfully", tasks, request, LocalDateTime.now());
+  }
+  
+  @Override
+  public ResponseEntity<StandardResponse<List<Task>>> getTaskByTeamId(UUID teamId, HttpServletRequest request) {
+    List<Task> tasks = taskRepo.findByTeamId(teamId);
+    if(tasks.isEmpty()){
+      return ResponseUtil.buildErrorMessage(HttpStatus.NOT_FOUND, "Task not found", request, LocalDateTime.now());
+      
+    }
+    return ResponseUtil.buildSuccessMessage(HttpStatus.OK, "Tasks fetched successfully", tasks, request, LocalDateTime.now());
+  }
+  
+  @Override
+  public ResponseEntity<StandardResponse<List<Task>>> getTaskByProjectId(UUID projectId, HttpServletRequest request) {
+    List<Task> tasks = taskRepo.findByProjectId(projectId);
+    if(tasks.isEmpty()){
+      return ResponseUtil.buildErrorMessage(HttpStatus.NOT_FOUND, "Task not found", request, LocalDateTime.now());
+      
+    }
+    return ResponseUtil.buildSuccessMessage(HttpStatus.OK, "Tasks fetched successfully", tasks, request, LocalDateTime.now());
+  }
+  
 }
